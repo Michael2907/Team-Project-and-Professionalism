ec92008e3b5987d1d0f8d3093f405253745a3d37
michael.clayton <w16013867@northumbria.ac.uk>
2020-02-26 11:04:24 +0000

    HTML and functions for login

--- js/controllers.js
+++ js/controllers.js
@@ -24,16 +24,43 @@
                     var modal = document.getElementById("loginModal");
 
 
-                    $scope.admin = false;
-                    $scope.userLogin = {};
+                    $scope.showAdmin = true;
+                    $scope.showGuest = false;
 
-                    $scope.login = function () {
+                    $scope.admin = {};
+                    $scope.guest = {};
+
+
+                    $scope.showAdminForm = function (){
+                        $scope.showAdmin = true;
+                        $scope.showGuest = false;
+                    }
+
+                    $scope.showGuestForm = function (){
+                        $scope.showAdmin = false;
+                        $scope.showGuest = true;
+                    }
+
+                    $scope.adminLogin = function () {
                         // Show
-                        modal.style.display = "block";
-                        dataService.login($scope.userLogin).then(  // then() is called when the promise is resolve or rejected
+                        dataService.adminLogin($scope.admin).then(  
                             function (response) {
                                 console.log(response);
+                            },
+                            function (err) {
+                                $scope.status = 'Unable to load data ' + err;
+                            },
+                            function (notify) {
+                                console.log(notify);
+                            }
+                        );
+                    };
 
+                    $scope.guestLogin = function () {
+                        // Show
+                        dataService.guestLogin($scope.guest).then(  
+                            function (response) {
+                                console.log(response);
                             },
                             function (err) {
                                 $scope.status = 'Unable to load data ' + err;
--- js/services.js
+++ js/services.js
@@ -24,16 +24,15 @@
                 function ($q, $http) {     // the parameters must be in the same order as the dependencies
 
 
-                    var urlBase = '/Web-Application-Integration/Assignment/Part1/api/';
+                    var urlBase = '/eam-Project-and-Professionalism/Team-Project-and-Professionalism-Front-end/api/';
 
-
-                    this.login = function (user) {
-                        let userJSON = { "username": user.username, "password": user.password };
+                    this.adminLogin = function (admin) {
+                        let adminJSON = { "username": admin.username, "password": admin.password };
 
 
                         var defer = $q.defer(),             // The promise
                             data = {                        // the data to be passed to the url
-                                action: 'login',
+                                action: 'adminLogin',
                             };
 
                         /**
@@ -43,7 +42,40 @@
                          * @param {object} config a configuration object, can contain parameters to pass, in this case we set cache to true
                          * @return {object} promise The call returns, not data, but a promise which only if the call is successful is 'honoured'
                          */
-                        $http.post(urlBase + data.action, userJSON, { cache: true }).                          // notice the dot to start the chain to success()
+                        $http.post(urlBase + data.action, adminJSON, { cache: true }).                          // notice the dot to start the chain to success()
+                            success(function (response) {
+                                defer.resolve({
+                                    data: response,         // create data property with value from response
+                                });
+                            }).                                                 // another dot to chain to error()
+                            error(function (err) {
+                                defer.reject(err);
+                            });
+
+                        // the call to getCourses returns this promise which is fulfilled 
+                        // by the .get method .success or .failure
+                        return defer.promise;
+
+                    };
+
+                    
+                    this.guestLogin = function (guest) {
+                        let guestJSON = { "fullName": guest.fullName, "numberPlate": guest.numberPlate, "email": guest.email };
+
+
+                        var defer = $q.defer(),             // The promise
+                            data = {                        // the data to be passed to the url
+                                action: 'guestLogin',
+                            };
+
+                        /**
+                         * make an ajax get call 
+                         * chain calls to .success and .error which will resolve or reject the promise
+                         * @param {string} urlBase The url to call, later we'll to this to pass parameters
+                         * @param {object} config a configuration object, can contain parameters to pass, in this case we set cache to true
+                         * @return {object} promise The call returns, not data, but a promise which only if the call is successful is 'honoured'
+                         */
+                        $http.post(urlBase + data.action, guestJSON, { cache: true }).                          // notice the dot to start the chain to success()
                             success(function (response) {
                                 defer.resolve({
                                     data: response,         // create data property with value from response
