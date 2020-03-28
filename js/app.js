c1fa27f9482f89f86ceca19cfa856029e8e803c5
Jack <jack.ocarroll@mediaworks.co.uk>
2020-03-28 14:23:22 +0000

    Add Reporting Modals

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
-"use strict";  // turn on javascript strict syntax mode
+"use strict"; // turn on javascript strict syntax mode
 
 /**
  * Start a new Application, a module in Angular
- * @param {string} ApplicationName a string which will be the name of the application 
+ * @param {string} ApplicationName a string which will be the name of the application
  *                 and, in fact, an object to which we add all other components
  * @param {array} dependencies An array of dependencies, the names are passed as strings
  */
-var assignmentApp = angular.module('AssignmentApp', [
-  'ngRoute',   // the only dependency at this stage, for routing
+var assignmentApp = angular.module("AssignmentApp", [
+  "ngRoute", // the only dependency at this stage, for routing
+  "ui.bootstrap",
+  "chart.js"
 ]);
 // note this fullstop where we chain the call to config
-assignmentApp.
-  config(
-    [
-      '$routeProvider',     // built in variable which injects functionality, passed as a string
-      function ($routeProvider) {
-        $routeProvider.
-          // not working :( 
-          when('/admin', {
-            templateUrl: 'js/partials/admin.html',
-            controller: 'AdminController'
-          }).
-          when('/login', {
-            templateUrl: 'js/partials/login.html',
-            controller: 'LoginController'
-          }).
-          when('/reports', {
-            templateUrl: 'js/partials/reporting.html',
-            controller: 'ReportingController'
-          }).
-          when('/', {
-            templateUrl: 'js/partials/landing.html',
-            controller: 'IndexController'
-          }).
-          otherwise({
-            redirectTo: '/',
-          });
-      }
-    ]
-  );  // end of config method 
+assignmentApp.config([
+  "$routeProvider", // built in variable which injects functionality, passed as a string
+  function($routeProvider) {
+    $routeProvider
+      // not working :(
+      .when("/admin", {
+        templateUrl: "js/partials/admin.html",
+        controller: "AdminController"
+      })
+      .when("/login", {
+        templateUrl: "js/partials/login.html",
+        controller: "LoginController"
+      })
+      .when("/reports", {
+        templateUrl: "js/partials/reporting.html",
+        controller: "ReportingController"
+      })
+      .when("/", {
+        templateUrl: "js/partials/landing.html",
+        controller: "IndexController"
+      })
+      .when("/modalTest", {
+        templateUrl: "js/partials/testModal.html",
+        controller: "ModalDemoCtrl"
+      })
+      .otherwise({
+        redirectTo: "/"
+      });
+  }
+]); // end of config method
---
+++ js/controllers/busiestDayModalController.js
@@ -0,0 +1,321 @@
+assignmentApp.controller("BusiestDayModalController", function(
+  dataService,
+  $uibModal,
+  $document
+) {
+  var $ctrl = this;
+  // dataService
+  //   .getActivities("2020-03-16T15:03:44.343Z", "2020-03-20T15:03:44.343Z")
+  //   .then(res => {
+  //     const {
+  //       data: { data: activies }
+  //     } = res;
+  //     console.log(res);
+
+  //   });
+  var activies = [
+    {
+      activityID: 0,
+      dateTimeEntered: "2020-03-16T15:03:44.343Z",
+      dateTimeExited: "2020-03-16T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 1,
+      dateTimeEntered: "2020-03-16T15:03:44.343Z",
+      dateTimeExited: "2020-03-16T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 2,
+      dateTimeEntered: "2020-03-17T15:03:44.343Z",
+      dateTimeExited: "2020-03-17T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 3,
+      dateTimeEntered: "2020-03-17T15:03:44.343Z",
+      dateTimeExited: "2020-03-17T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 4,
+      dateTimeEntered: "2020-03-18T15:03:44.343Z",
+      dateTimeExited: "2020-03-18T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 5,
+      dateTimeEntered: "2020-03-18T15:03:44.343Z",
+      dateTimeExited: "2020-03-18T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 6,
+      dateTimeEntered: "2020-03-19T15:03:44.343Z",
+      dateTimeExited: "2020-03-19T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 7,
+      dateTimeEntered: "2020-03-19T15:03:44.343Z",
+      dateTimeExited: "2020-03-19T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 8,
+      dateTimeEntered: "2020-03-19T15:03:44.343Z",
+      dateTimeExited: "2020-03-19T15:03:44.343Z",
+      userID: 0
+    },
+    {
+      activityID: 9,
+      dateTimeEntered: "2020-03-20T15:03:44.343Z",
+      dateTimeExited: "2020-03-20T15:03:44.343Z",
+      userID: 0
+    }
+  ];
+
+  var activityDays = activies.map(activity =>
+    new Date(activity.dateTimeEntered).getDay()
+  );
+
+  $ctrl.data = [
+    [
+      activityDays.filter(day => day === 1).length,
+      activityDays.filter(day => day === 2).length,
+      activityDays.filter(day => day === 3).length,
+      activityDays.filter(day => day === 4).length,
+      activityDays.filter(day => day === 5).length
+    ]
+  ];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  $ctrl.series = ["Days Of the Week"];
+
+  $ctrl.format = "dd-MMMM-yyyy";
+
+  $ctrl.setDate = function(year, month, day) {
+    $ctrl.startDt = new Date(year, month, day);
+    $ctrl.endDt = new Date(year, month, day);
+  };
+
+  $ctrl.dateOptions = {
+    dateDisabled: disabled,
+    formatYear: "yy",
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  $ctrl.inlineOptions = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: true
+  };
+
+  $ctrl.todaySDt = function() {
+    $ctrl.startDt = new Date();
+  };
+  $ctrl.todaySDt();
+
+  $ctrl.todayEDt = function() {
+    $ctrl.endDt = new Date();
+  };
+  $ctrl.todayEDt();
+
+  $ctrl.popup1 = {
+    opened: false
+  };
+  $ctrl.popup2 = {
+    opened: false
+  };
+
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === "day" && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === "day") {
+      var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+      for (var i = 0; i < $ctrl.events.length; i++) {
+        var currentDay = new Date($ctrl.events[i].date).setHours(0, 0, 0, 0);
+
+        if (dayToCheck === currentDay) {
+          return $ctrl.events[i].status;
+        }
+      }
+    }
+
+    return "";
+  }
+
+  $ctrl.open = function(size, parentSelector) {
+    var parentElem = parentSelector
+      ? angular.element(
+          $document[0].querySelector(".modal-demo " + parentSelector)
+        )
+      : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: "busiestDayTitle",
+      ariaDescribedBy: "busiestDayBody",
+      templateUrl: "busiestDayContent.html",
+      controller: "ModalInstanceCtrl",
+      controllerAs: "$ctrl",
+      size: size,
+      appendTo: parentElem,
+      resolve: {
+        series: function() {
+          return $ctrl.series;
+        },
+        labels: function() {
+          return $ctrl.labels;
+        },
+        data: function() {
+          return $ctrl.data;
+        },
+        format: function() {
+          return $ctrl.format;
+        },
+        startDt: function() {
+          return $ctrl.startDt;
+        },
+        endDt: function() {
+          return $ctrl.endDt;
+        },
+        popup1: function() {
+          return $ctrl.popup1;
+        },
+        popup2: function() {
+          return $ctrl.popup2;
+        }
+      }
+    });
+
+    modalInstance.result.then(
+      function(selectedItem) {
+        $ctrl.selected = selectedItem;
+      },
+      function() {
+        $log.info("Modal dismissed at: " + new Date());
+      }
+    );
+  };
+
+  $ctrl.openComponentModal = function() {
+    $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      component: "busiestDayComponent",
+      resolve: {
+        series: function() {
+          return $ctrl.series;
+        },
+        labels: function() {
+          return $ctrl.labels;
+        },
+        data: function() {
+          return $ctrl.data;
+        },
+        format: function() {
+          return $ctrl.format;
+        },
+        startDt: function() {
+          return $ctrl.startDt;
+        },
+        endDt: function() {
+          return $ctrl.endDt;
+        },
+        popup1: function() {
+          return $ctrl.popup1;
+        },
+        popup2: function() {
+          return $ctrl.popup2;
+        }
+      }
+    });
+  };
+
+  $ctrl.toggleAnimation = function() {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+assignmentApp.controller("ModalInstanceCtrl", function(
+  $uibModalInstance,
+  series,
+  labels,
+  data,
+  format,
+  startDt,
+  endDt,
+  popup1,
+  popup2
+) {
+  var $ctrl = this;
+
+  $ctrl.series = series;
+  $ctrl.labels = labels;
+  $ctrl.data = data;
+  $ctrl.format = format;
+  $ctrl.startDt = startDt;
+  $ctrl.endDt = endDt;
+  $ctrl.popup1 = popup1;
+  $ctrl.popup2 = popup2;
+
+  $ctrl.ok = function() {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function() {
+    $uibModalInstance.dismiss("cancel");
+  };
+
+  $ctrl.open1 = function() {
+    console.log("is it here");
+    $ctrl.popup1.opened = true;
+  };
+  $ctrl.open2 = function() {
+    console.log("is it here");
+    $ctrl.popup2.opened = true;
+  };
+});
+
+// Please note that the close and dismiss bindings are from $uibModalInstance.
+
+assignmentApp.component("busiestDayComponent", {
+  templateUrl: "busiestDayContent.html",
+  bindings: {
+    resolve: "<",
+    close: "&",
+    dismiss: "&"
+  },
+  controller: function() {
+    var $ctrl = this;
+
+    $ctrl.$onInit = function() {
+      $ctrl.items = $ctrl.resolve.items;
+      $ctrl.selected = {
+        item: $ctrl.items[0]
+      };
+    };
+
+    $ctrl.ok = function() {
+      $ctrl.close({ $value: $ctrl.selected.item });
+    };
+
+    $ctrl.cancel = function() {
+      $ctrl.dismiss({ $value: "cancel" });
+    };
+  }
+});
